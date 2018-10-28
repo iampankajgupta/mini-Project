10c46266c99f043ca2fa7f9b5fadb5eb55138452
iampankajgupta <itsmeguptapankaj@gmail.com>
2018-10-28 23:24:21 +0530

    Create secondstyle.php
    second file style sheet

---
+++ secondstyle.php
@@ -0,0 +1,25 @@
+
+form{
+width:30%;
+margin:-70px auto;
+border-radius:10px;
+padding:10px;
+background:#FFFFFF;
+}
+
+.sizetext{
+	size:45;
+width:89%;
+height:10px;
+padding:10px;
+border; 2px solid #black;	
+margin:auto;		
+}
+h1{
+	font-size:80;
+	margin-top:100px;
+	background-color:white:
+	
+	
+	
+}
