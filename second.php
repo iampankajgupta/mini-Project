4ea18ea77fa788f3b796e301c17ad65310b1cddf
iampankajgupta <itsmeguptapankaj@gmail.com>
2018-10-28 23:23:29 +0530

    Create second.php
    second file

---
+++ second.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>
+Welcome
+</title>
+<link rel = "stylesheet" type = "text/css " href = "style.css" >
+
+</head>
+<body BGCOLOR = "#239B56">
+<center><h1> Welcome </h1></center><br><br>
+<form method = "Post" action = "">
+<input type ="text" name = "task" size = "100"class  = "sizetext" placeholder = "Enter your tasks...">
+<button  type ="submit" class "button" name = "submit"> Add </button>
+</form>
+</body>
+
+</html>
