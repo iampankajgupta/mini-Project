a6af595f210ca4d13f73c02e651684fb1e321a4b
iampankajgupta <itsmeguptapankaj@gmail.com>
2018-10-30 09:55:11 +0530

    final first page

---
+++ finalfirstpage.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>
+To Do List
+</title>
+<style>
+form{
+background-color : white;
+margin:uto;
+padding:5%;
+}
+</style>
+</head>
+<body BGCOLOR = "#239B56">
+<font size = "8"><center> TO DO LIST </center></font>
+<fieldset>
+<legend><center> Login Page </center></legend>
+<form align = "center">
+Name:<input type  = "text" name = "username" placeholder = "Name"  ><br><br>
+Email:<input type = "text" name = "email" placeholder = "email address"><br><br>
+Password:<input type = "text" name = "password"  placeholder = "password" ><br><br>
+<button type= "submit" namae = "submit" > Submit</button>
+</form>
+</body>
+</html>
